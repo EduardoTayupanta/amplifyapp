10cd0d9657befbf326f8d02c9179f64361af6285
Eduardo Tayupanta <wetayupanta@gmail.com>
2020-11-02 12:38:29 -0500

    change folder

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "../styles/App.css";
 import { API, Storage } from "aws-amplify";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
+import { listNotes } from "../graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+} from "../graphql/mutations";
 
 const initialFormState = { name: "", description: "" };
 
