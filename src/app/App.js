6cea34169fb506064a1ea16edaf7a3cf777e71a7
Eduardo Tayupanta <wetayupanta@gmail.com>
2020-11-02 12:54:47 -0500

    test Auth

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import logo from "../styles/logo.svg";
 import "../styles/App.css";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { AmplifySignOut } from "@aws-amplify/ui-react";
+
+import { Auth } from "./auth";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
+    <Auth>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!</h1>
+        </header>
+        <AmplifySignOut />
+      </div>
+    </Auth>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
---
+++ src/app/auth/Auth.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+
+function Auth() {
+  return <AmplifyAuthenticator />;
+}
+
+export default Auth;
